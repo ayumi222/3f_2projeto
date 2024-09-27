b88ee003370ec2dbead88d1912ef171f97eb7cf5
ayumi222 <108939482+ayumi222@users.noreply.github.com>
2024-09-27 08:54:40 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,3 +1,13 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
-console.log(url);
+async function visualizaDadosGlobais(){
+    const res = await fetch(url);
+    const dados = await res.json();
+    console.log(dados);
+
+const paragrafo = document.createElement('p')
+paragrafo.classList.add('grafico-texto')
+const conteiner = document.getElementById('grafico-conteiner')
+}
+
+visualizaDadosGlobais();
