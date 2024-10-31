afe2acb12f6e467e31bbcc91e534df855c4ede19
ayumi222 <108939482+ayumi222@users.noreply.github.com>
2024-10-31 09:49:40 -0300

    Add files via upload

--- graficos/quantidadeDeUsuarios.js
+++ graficos/quantidadeDeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS } from "./comum.js";
+import { getCSS, tickfont } from "./comum.js";
 async function quantidadeDeUsuarios(){
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
     const res = await fetch(url);
@@ -19,13 +19,32 @@ async function quantidadeDeUsuarios(){
     const layout = {
         plot_bgcolor: getCSS('--cor-de-fundo'),
         paper_bgcolor: getCSS('--cor-de-fundo'),
-        title: 'Redes sociais com mais usúarios no mundo.',
+        title: {
+            text:'Redes sociais com mais usúarios no mundo.',
         x:0,
         font:{
             color: getCSS('--cor-primaria'),
             family: getCSS('--font'),
             size: 30
         }
+    },
+    xaxis:{
+        tickfont: tickfont,
+        title: {
+            text: 'Nome das redes sociais',
+            font:{
+                color: getCSS('--cor-secundaria')
+            }
+        }
+    },
+    yaxis:{
+        title:{
+            text: 'Milhões de usuários ativos',
+            font:{
+                color: getCSS('--cor-secundaria')
+            }
+        }
+    }
     }
 
     const grafico = document.createElement('div');
